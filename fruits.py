9d2924ee97097a1dd1d27a2b554835a824788ae6
Ryan Wolff <ryan.h.wolff@gmail.com>
2023-09-08 10:13:58 -0600

    Removed comma after last fruit

--- fruits.py
+++ fruits.py
@@ -1,2 +1,2 @@
-fruits = ['apple', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape', 'pumpkin',]
+fruits = ['apple', 'dragonfruit', 'peach', 'banana', 'grape', 'apple', 'peach', 'watermelon', 'grape', 'grape', 'pumpkin']
 fruits = set(fruits)
